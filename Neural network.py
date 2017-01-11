37237f7107ea04678960375973eec509eee626e0
Roger Peralta <aranibar.roger@gmail.com>
2017-01-10 20:48:57 -0500

    - Done

--- Neural network.py
+++ Neural network.py
@@ -1,17 +1,16 @@
-import tensorflow as tf
+import tensorflow
+import numpy
+import random
 
-train_feat,train_label,test_feat,test_label = create_feature_set('car.data','car-prueba.data') 
 
-n_nodes_hidden_layer_1 = 3
-n_nodes_hidden_layer_2 = 4
-n_nodes_hidden_layer_3 = 4
-n_nodes_hidden_layer_4 = 3
+n_layer_1 = 3
+n_layer_2 = 4
+n_layer_3 = 4
+n_layer_4 = 3
 
 n_labels = 1
 
 iterations = 10
-
-label = {'unacc': [0], 'acc': [1], 'good': [2], 'vgood':[3]}
 
 buying = {'low': 0, 'med': 1, 'high': 2, 'vhigh':3}
 maint = {'low': 0, 'med': 1, 'high': 2, 'vhigh':3}
@@ -19,30 +18,18 @@ doors = {'2': 2, '3': 3, '4': 4, '5more':5}
 person = {'2': 2, '4': 4, 'more': 6}
 lugboot = {'small': 0, 'med': 1, 'big': 2}
 safety = {'low': 0, 'med': 1, 'high': 2}
-
-x = tf.placeholder('float')
-y = tf.placeholder('float')
-
-hidden_1_layer = {'weights': tf.Variable(tf.random_normal([len(train_feat[0]),n_nodes_hidden_layer_1])), 'bias':tf.Variable(tf.random_normal([n_nodes_hidden_layer_1]))}
-
-hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hidden_layer_1,n_nodes_hidden_layer_2])), 'bias':tf.Variable(tf.random_normal([n_nodes_hidden_layer_2]))}
-
-hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hidden_layer_2,n_nodes_hidden_layer_3])), 'bias':tf.Variable(tf.random_normal([n_nodes_hidden_layer_3]))}
-
-hidden_4_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hidden_layer_3,n_nodes_hidden_layer_4])), 'bias':tf.Variable(tf.random_normal([n_nodes_hidden_layer_4]))}
-
-output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hidden_layer_4,n_labels])), 'bias':tf.Variable(tf.random_normal([n_labels]))}
+labels = {'unacc': [0], 'acc': [1], 'good': [2], 'vgood':[3]}
 
 
 def read_file(doc):
 	feature_set = []
-	fs = []
-	with open(doc,'r') as f:
-		fs+=[line.split(',') for line in f] 
-	for line in fs:
+	file_lines = []
+	with open(doc,'r') as file:
+		file_lines += [line.split() for line in file] 
+	for line in file_lines:
 		feature_vect = [buying[line[0]], maint[line[1]], doors[line[2]], person[line[3]], lugboot[line[4]], safety[line[5]]]
-		feature_label = label[line[6]]
-		feature_line =[feature_vect, feature_label]
+		feature_label = labels[line[6]]
+		feature_line = [feature_vect, feature_label]
 		feature_set.append(feature_line)
 	
 	return feature_set
@@ -56,42 +43,59 @@ def create_feature_set(trainingdataset, testdataset):
 	training_features = numpy.array(training_features)
 	test_features = numpy.array(test_features)
 	
-	train_feat = list(training_features[:,0])
-	train_label = list(training_features[:,1])
-	test_feat = list(test_features[:,0])
-	test_label = list(test_features[:,1])
+	train_feat = list(training_features[:, 0])
+	train_label = list(training_features[:, 1])
+	test_feat = list(test_features[:, 0])
+	test_label = list(test_features[:, 1])
 
 
-	return train_feat,train_label,test_feat,test_label
+	return train_feat, train_label, test_feat, test_label
+
+
+train_input = tensorflow.placeholder('float')
+test_output = tensorflow.placeholder('float')
+
+train_feat,train_label,test_feat,test_label = create_feature_set('car.data','car-prueba.data')
+
+hidden_layer_1 = {'weights': tensorflow.Variable(tensorflow.random_normal([len(train_feat[0]), n_layer_1])), 'bias': tensorflow.Variable(tensorflow.random_normal([n_layer_1]))}
+
+hidden_layer_2 = {'weights': tensorflow.Variable(tensorflow.random_normal([n_layer_1, n_layer_2])), 'bias': tensorflow.Variable(tensorflow.random_normal([n_layer_2]))}
+
+hidden_layer_3 = {'weights': tensorflow.Variable(tensorflow.random_normal([n_layer_2, n_layer_3])), 'bias': tensorflow.Variable(tensorflow.random_normal([n_layer_3]))}
+
+hidden_layer_4 = {'weights': tensorflow.Variable(tensorflow.random_normal([n_layer_3, n_layer_4])), 'bias': tensorflow.Variable(tensorflow.random_normal([n_layer_4]))}
+
+output_layer = {'weights': tensorflow.Variable(tensorflow.random_normal([n_layer_4, n_labels])), 'bias': tensorflow.Variable(tensorflow.random_normal([n_labels]))}
+
 
 def neural_network_model(data):
 	
-	layer_1 = tf.add(tf.matmul(data,hidden_1_layer['weights']),hidden_1_layer['bias'])
-	layer_1 = tf.nn.relu(l1)
+	layer_1 = tensorflow.add(tensorflow.matmul(data, hidden_layer_1['weights']), hidden_layer_1['bias'])
+	layer_1 = tensorflow.nn.relu(layer_1)
 	
-	layer_2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']),hidden_2_layer['bias'])
-	layer_2 = tf.nn.relu(l2)
+	layer_2 = tensorflow.add(tensorflow.matmul(layer_1, hidden_layer_2['weights']), hidden_layer_2['bias'])
+	layer_2 = tensorflow.nn.relu(layer_2)
 	
-	layer_3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']),hidden_3_layer['bias'])
-	layer_3 = tf.nn.relu(l3)
+	layer_3 = tensorflow.add(tensorflow.matmul(layer_2, hidden_layer_3['weights']), hidden_layer_3['bias'])
+	layer_3 = tensorflow.nn.relu(layer_3)
 
-	layer_4 = tf.add(tf.matmul(l3,hidden_4_layer['weights']),hidden_4_layer['bias'])
-	layer_4 = tf.nn.relu(l4)
+	layer_4 = tensorflow.add(tensorflow.matmul(layer_3, hidden_layer_4['weights']), hidden_layer_4['bias'])
+	layer_4 = tensorflow.nn.relu(layer_4)
 
-	layer_output = tf.add(tf.matmul(l4,output_layer['weights']),output_layer['bias'])
+	layer_output = tensorflow.add(tensorflow.matmul(layer_4, output_layer['weights']), output_layer['bias'])
 	
 	return layer_output
 
-def train_neural_network(x):
+def train_neural_network(train_input):
 	
-	prediction = neural_network_model(x)
-	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction,y))
-	optimizer = tf.train.AdamOptimizer().minimize(cost) 
+	prediction = neural_network_model(train_input)
+	cost = tensorflow.reduce_mean(tensorflow.nn.softmax_cross_entropy_with_logits(prediction,y))
+	optimizer = tensorflow.train.AdamOptimizer().minimize(cost) 
 
-	with tf.Session() as sess:
-		sess.run(tf.initialize_all_variables())
+	with tensorflow.Session() as sess:
+		sess.run(tensorflow.initialize_all_variables())
 	    
-		for iteration in range(iterations):
+		for IT in range(iterations):
 			loss = 0
 			I=0
 			while I < len(train_feat):
@@ -100,15 +104,16 @@ def train_neural_network(x):
 				batch_feat = numpy.array(train_feat[start:end])
 				batch_label = numpy.array(train_label[start:end])
 
-				_, K = sess.run([optimizer, cost], feed_dict={x: batch_feat, y: batch_label})
+				_, K = sess.run([optimizer, cost], feed_dict = {train_input: batch_feat, y: batch_label})
 				loss += K
 				I += 1
 				
-			print('Iteration', iteration+1,'loss:',loss)
+			print('Iteration', IT + 1,'loss:',loss)
 		
-		correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-		accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-		print('Accuracy:',accuracy.eval({x:test_feat, y:test_label}))
+		correct = tensorflow.equal(tensorflow.argmax(prediction, 1), tensorflow.argmax(test_output, 1))
+		accuracy = tensorflow.reduce_mean(tensorflow.cast(correct, 'float'))
+
+		print('Accuracy:', accuracy.eval({train_input: test_feat, test_output: test_label}))
 
 
-train_neural_network(x)
+train_neural_network(train_input)
